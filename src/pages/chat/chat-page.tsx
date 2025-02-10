de9f641bc373112c3b6ade6dcf0dc721f6bbc70c
siddiq <siddiqahmed882@gmail.com>
2025-02-10 23:05:59 +0500

    add searchbar for chat

--- src/pages/chat/chat-page.tsx
+++ src/pages/chat/chat-page.tsx
@@ -1,5 +1,6 @@
 import { Outlet, Route, Routes, useParams } from 'react-router-dom';
 
+import { Input } from '@/components/ui/input';
 import { ScrollArea } from '@/components/ui/scroll-area';
 import { useIsMobile } from '@/hooks/use-mobile';
 import { cn } from '@/lib/utils';
@@ -28,14 +29,23 @@ function ChatSidebar() {
   return (
     <ScrollArea
       className={cn(
-        'w-1/4 min-w-80 h-full px-2 py-2 md:py-4 md:px-4',
+        'w-1/4 min-w-80 h-full',
         isMobile ? (id ? 'hidden' : 'w-full') : ''
       )}
     >
-      <div className="flex flex-col gap-2">
-        {contacts.map((contact) => (
-          <ChatListItem key={contact.user.id} contact={contact} />
-        ))}
+      {/* Search bar */}
+      <div className="px-2 py-2 md:py-4 md:px-4">
+        <Input
+          placeholder="Search contacts..."
+          className="bg-transparent mb-4"
+          onKeyDown={(e) => e.key === 'Enter' && console.log('Search')}
+        />
+
+        <div className="flex flex-col gap-2">
+          {contacts.map((contact) => (
+            <ChatListItem key={contact.user.id} contact={contact} />
+          ))}
+        </div>
       </div>
     </ScrollArea>
   );
