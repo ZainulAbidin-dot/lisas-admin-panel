d0e1e7b7e65b49f887f5bf9d47274a3ef22d66bd
ZainulAbidin-dot <zchxvr@gmail.com>
2025-02-21 23:33:47 +0300

    user error

--- vercel.config.ts
+++ vercel.config.ts
@@ -1,13 +1,7 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: "./", // Ensures correct asset paths
-  build: {
-    outDir: "dist", // Vercel serves from "dist"
-    sourcemap: true, // Optional: Helps with debugging
-  },
-  server: {
-    port: 5173, // Default Vite port
-    open: true,
-  },
-});
+  plugins: [react()],
+})
