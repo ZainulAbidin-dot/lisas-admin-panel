cc30ff60a36cfd45b47c5919539672a5b0fb7a02
siddiq <siddiqahmed882@gmail.com>
2025-02-23 11:55:34 +0500

    ...

--- src/api/axios-instance.ts
+++ src/api/axios-instance.ts
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-const BASE_URL = import.meta.env.VITE_API_URL;
+const BASE_URL = `${import.meta.env.VITE_API_URL}/ap`;
 
 export const axiosInstance = axios.create({
-  baseURL: `${BASE_URL}/api`,
+  baseURL: BASE_URL
 });
 
 export const axiosPrivateInstance = axios.create({
